f2d31bfc6d25223c24b9acd31b82409b5c037459
Jennifer Vero Baxter <45215576+jennyvb143@users.noreply.github.com>
2020-09-18 20:35:52 -0400

    Updates

---
+++ precache-manifest.26a7441314735f29927cea0298a6cd15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea55f7f0d8019e6458569d6f02a69b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c440d8d94210e1d9bc4",
+    "url": "/static/js/2.41a6f440.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.41a6f440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f54a468f54084c79f065",
+    "url": "/static/js/main.3a7c2264.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PortfolioSite/precache-manifest.777bf1182e676fdc3766ee06e68a5987.js"
+  "/precache-manifest.26a7441314735f29927cea0298a6cd15.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PortfolioSite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
