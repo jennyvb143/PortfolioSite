ecd72f659092b37177ca0ab5fe93462e17273531
Jennifer Vero Baxter <45215576+jennyvb143@users.noreply.github.com>
2021-12-18 16:19:26 -0500

    creating dev branch

---
+++ portfolio/build/precache-manifest.565945190ff10256935e925eacde5490.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b384c1737647d2c8da3124519e24c77b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2430d670cfec288ba643",
+    "url": "/static/css/main.d001e81c.chunk.css"
+  },
+  {
+    "revision": "8bd941c09b8f7e422dd3",
+    "url": "/static/js/2.b68a06d0.chunk.js"
+  },
+  {
+    "revision": "73772f9da71bcbf46f095fde59eb43d5",
+    "url": "/static/js/2.b68a06d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2430d670cfec288ba643",
+    "url": "/static/js/main.fdfca9c3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "1e3c648458d7d48daf91a878fcee7d4f",
+    "url": "/static/media/JenniferBaxter-Resume.1e3c6484.pdf"
+  },
+  {
+    "revision": "d3792a0348c987b0c16fda149e4cca8c",
+    "url": "/static/media/bed-baby-card.d3792a03.png"
+  },
+  {
+    "revision": "99c15e1bada207e8b518d24017d5471b",
+    "url": "/static/media/bed-baby-main-1.99c15e1b.jpg"
+  },
+  {
+    "revision": "81ee643ce44832949a2a7bf76997532c",
+    "url": "/static/media/bed-baby-main-2.81ee643c.jpg"
+  },
+  {
+    "revision": "cd8f4ffff53138fd945e163d0fa067d9",
+    "url": "/static/media/bed-baby-main-3.cd8f4fff.jpg"
+  },
+  {
+    "revision": "6e83f948d5c33f85e175b2b730b054ba",
+    "url": "/static/media/bed-baby-main-4.6e83f948.jpg"
+  },
+  {
+    "revision": "f0947ccd5bcaf7e8261b6b69f70d60c2",
+    "url": "/static/media/bed-baby-main-6.f0947ccd.jpg"
+  },
+  {
+    "revision": "7bb16d28f9d054d0ffa529b13d227171",
+    "url": "/static/media/els-card.7bb16d28.png"
+  },
+  {
+    "revision": "7aae60530ee2fa9e1ffc297b291e6992",
+    "url": "/static/media/freitag-ads.7aae6053.jpg"
+  },
+  {
+    "revision": "6f845723d9061070817bb7ba6a0f677f",
+    "url": "/static/media/freitag-card.6f845723.png"
+  },
+  {
+    "revision": "adead9e1f3bfc2b32a91742954bb74a8",
+    "url": "/static/media/jennifer-baxter-avatar.adead9e1.png"
+  },
+  {
+    "revision": "511c6db4a5710306d92a5cc65517e047",
+    "url": "/static/media/jenniferbaxter-jvbaxter.com-logo.511c6db4.png"
+  }
+]);
